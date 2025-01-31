3e4cbacf473046594dbdddd87e85490fb8af17b9
visal.naqvi <visal.naqvi@electrifai.com>
2025-01-31 16:21:45 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -34,37 +34,10 @@ export default function Home() {
             height={38}
             priority
           />
-          <ol>
-            <li>
-              Get started by editing <code>pages/index.js</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
 
           <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+            <p>Domain Available to sell.</p>
+            <p>Email: rajiulsingh00@gmail.com</p>
           </div>
         </main>
         <footer className={styles.footer}>
